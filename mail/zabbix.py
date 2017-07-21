8de92378fbce13aae177c6da6dcbf82f79066814
hunter <mortimer2015@hotmail.com>
2017-07-21 17:31:45 +0800

    io

--- mail/zabbix.py
+++ mail/zabbix.py
@@ -1,21 +1,22 @@
-# coding: utf-8
+#!/usr/bin/python3
+# coding: gb2312
 import smtplib
-import time
+import sys
 from email.mime.text import MIMEText
 # from email.header import Header
 
-nowtime = time.strftime('%Y-%m-%d-%H:%M', time.localtime(time.time()))
+_, subject, *msgbody = sys.argv
 sender = 'mortimer2017@163.com'
 receiver = ['328624847@qq.com']
-subject = 'ServerError at {}'.format(nowtime)
+# subject = 'ServerError at {}'.format(nowtime)
 smtpserver = 'smtp.163.com'
 username = sender
 password = 'wyc12345'
 
 for i in receiver:
-    msg = MIMEText('ServerError at {} please fix it'.format(nowtime), 'plain',
-                   'utf-8')
-    msg['Subject'] = subject
+    msg = MIMEText(' '.join(msgbody), 'plain',
+                   'gb2312')
+    msg['Subject'] = str(subject)
     msg['From'] = sender
     msg['To'] = i
     smtp = smtplib.SMTP()
