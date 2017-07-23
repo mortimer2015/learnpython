2e7950d5faa4074efd4dab504781d5c463ce42c5
mortimer2015@hotmail.com <mortimer2015@hotmail.com>
2017-07-23 16:31:19 +0800

    hanshu

--- mail/testm.py
+++ mail/testm.py
@@ -1,17 +1,19 @@
 # coding: utf-8
 import smtplib
+import time
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'mortimer2017@163.com'
-receiver = ['328624847@qq.com']
-subject = '服务器故障'
-smtpserver = 'smtp.163.com'
+nowtime = time.strftime('%Y-%m-%d-%H:%M', time.localtime(time.time()))
+sender = 'guocai666@21cn.com'
+receiver = ['mortimer2017@163.com']
+subject = 'ServerError at {}'.format(nowtime)
+smtpserver = 'smtp.21cn.com'
 username = sender
-password = 'wyc12345'
+password = 'l7hqfr'
 
 for i in receiver:
-    msg = MIMEText('请查看监控，判断问题并处理', 'plain', 'utf-8')
+    msg = MIMEText('ServerError at {} please fix it'.format(nowtime), 'plain', 'utf-8')
     msg['Subject'] = Header(subject, 'utf-8')
     msg['From'] = sender
     msg['To'] = i
